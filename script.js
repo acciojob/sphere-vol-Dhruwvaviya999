3d75aee75199178af768b34ad6a46629e9b17490
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-04-17 19:43:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+	let radius = document.getElementById("radius").value;
+
+	if (isNaN(radius)) {
+        document.getElementById("volume").value = "NaN";
+        return;
+    }
+	
+	
+	let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    document.getElementById("volume").value = volume.toFixed(4); 
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
